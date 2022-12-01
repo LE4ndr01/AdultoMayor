2aa87af60135d534feaccc4612e9be83dbca71bf
Leandro <lea.suarez@duocuc.cl>
2022-11-30 23:19:45 -0300

    con

--- adulto/Arquitectura/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . views import index, contacto, nosotros
-urlpatterns = [
-    path('', index, name="index"),
-    path('contacto/', contacto, name="contacto"),
-    path('nosotros/', nosotros, name="nosotros"),
-]
