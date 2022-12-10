3df17bd2888fa938008baa84ba0b10805c049d3b
Leandro <lea.suarez@duocuc.cl>
2022-12-10 01:41:17 -0300

    con

--- adulto/app/urls.py
+++ adulto/app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from . views import index, contacto, evaluacion, postulaciones, talleres
+from . views import index, contacto, evaluacion, postulaciones, talleres, login, evaluacion, postulaciones
 urlpatterns = [
     path('', index, name="home"),
     path('contacto/', contacto, name="contacto"),
     path('evaluacion/', evaluacion, name="evaluacion"),
     path('postulaciones/', postulaciones, name="postulaciones"),
     path('talleres/', talleres, name="talleres"),
+    path('login/', login, name="login"),
+    path('evaluacion/', evaluacion, name="evaluacion"),
+    path('postulaciones/', postulaciones, name="postulaciones"),
 ]
--- adulto/app/views.py
+++ adulto/app/views.py
@@ -5,10 +5,16 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'app/home.html')
 def contacto(request):
-    return render(request, 'app/about.html')
+    return render(request, 'app/contacto.html')
 def evaluacion(request):
     return render(request, 'app/evaluacion.html')
 def postulaciones(request):
     return render(request, 'app/postulaciones.html')
 def talleres(request):
     return render(request, 'app/talleres.html')
+def login(request):
+    return render(request, 'app/login.html')
+def evaluacion(request):
+    return render(request, 'app/evaluacion.html')
+def postulaciones(request):
+    return render(request, 'app/postulaciones.html')    
