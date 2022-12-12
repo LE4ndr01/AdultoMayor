c6074170557ce38e6ccea92e612e71df189dbef4
leandro <lea.suarez@duocuc.cl>
2022-12-11 23:27:43 -0300

    con

--- adulto/app/admin.py
+++ adulto/app/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
-from .models import Usuario, Instructor
+from .models import Instructor,Taller
 # Register your models here.
 
-admin.site.register(Usuario)
+# Clase taller
+
+class TallerAdmin(admin.ModelAdmin):
+    list_display = ["nombre","descripcion","instructor"]
+    list_editable = ["descripcion"]
+    search_fields =["nombre"]
+    list_filter = ["instructor","nuevo"]
+    list_per_page = 10
+    
+
+
+
+
 admin.site.register(Instructor)
+admin.site.register(Taller)
--- adulto/app/models.py
+++ adulto/app/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class Usuario(models.Model):
-    nombre = models.CharField(max_length=50)
-    apellido = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.nombre
 class Instructor(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
+    
 
+    def __str__(self):
+        return self.nombre
+    
+class Taller(models.Model):
+    nombre=models.CharField(max_length=50)
+    descripcion=models.TextField(max_length=250)
+    Instructor=models.ForeignKey(Instructor,on_delete=models.CASCADE)
+    imagen = models.ImageField(upload_to="taller", null=True)
+    cupos = models.IntegerField()
+    
+    
     def __str__(self):
         return self.nombre
